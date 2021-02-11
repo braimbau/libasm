2e35ec7c4636904bf48e7e30160bd4af0d5f2143
billyboy <braimbau@student.42.fr>
2021-02-11 20:36:27 +0100

    push vm

--- include/libasm.h
+++ include/libasm.h
@@ -8,6 +8,7 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <string.h>
+# include <errno.h>
 
 ssize_t     ft_read(int fildes, void *buf, size_t nbyte);
 int		    ft_strcmp(const char *s1, const char *s2);
--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,11 +1,17 @@
+		extern __errno_location
 		global ft_write
 section .text
 
 ft_write:
 		mov rax, 1
 		syscall
-		jc error
+		cmp rax, 0
+		jl error
 		ret
 error:
+		mov rdi, rax
+		neg rdi
+		call __errno_location
+		mov [rdi], rax
 		mov rax, -1
 		ret
